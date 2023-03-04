f09f0be36b705ab4e0c7e90840d76c7850b1cdf0
RaihanNagata <122301807+RaihanNagata@users.noreply.github.com>
2023-03-04 11:44:08 +0700

    Add files via upload

--- 2b.php
+++ 2b.php
@@ -1,51 +1,8 @@
 <?php
-for ($angka = 1; $angka <= 1; $angka++) {
-    echo "$angka";
-}
-?>
-<br><?php
-    for ($angka = 1; $angka <= 2; $angka++) {
-        echo "$angka";
+$jumlah = '10';
+for ($nilai = 1; $nilai <= $jumlah; $nilai++) {
+    for ($angka = 1; $angka <= $nilai; $angka++) {
+        echo "$angka ";
     }
-    ?>
-
-<br> <?php
-        for ($angka = 1; $angka <= 3; $angka++) {
-            echo "$angka";
-        }
-        ?>
-<br> <?php
-        for ($angka = 1; $angka <= 4; $angka++) {
-            echo "$angka";
-        }
-        ?>
-<br> <?php
-        for ($angka = 1; $angka <= 5; $angka++) {
-            echo "$angka";
-        }
-        ?>
-<br> <?php
-        for ($angka = 1; $angka <= 6; $angka++) {
-            echo "$angka";
-        }
-        ?>
-<br> <?php
-        for ($angka = 1; $angka <= 7; $angka++) {
-            echo "$angka";
-        }
-        ?>
-<br> <?php
-        for ($angka = 1; $angka <= 8; $angka++) {
-            echo "$angka";
-        }
-        ?>
-<br> <?php
-        for ($angka = 1; $angka <= 9; $angka++) {
-            echo "$angka";
-        }
-        ?>
-<br> <?php
-        for ($angka = 1; $angka <= 10; $angka++) {
-            echo "$angka";
-        }
-        ?>
+    echo "<br>";
+}
